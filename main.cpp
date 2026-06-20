0453741bcea1dea212fac6554454a53d315dbd43
Andezion <245122@edu.p.lodz.pl>
2026-06-20 16:16:52 +0200

    add: added new stuff

--- main.cpp
+++ main.cpp
@@ -1,9 +1,69 @@
-#include <iostream>
 #include <windows.h>
 
-int WINAPI WinMain( HINSTANCE hinstance, HINSTANCE hprevInstance, LPSTR lpCmdLine, int nCmdShow )
+LPSTR class_name = "Test Shit";
+MSG message;
+
+LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam );
+
+int WINAPI WinMain(const HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, const int nShowCmd )
 {
-    auto responce = MessageBox( NULL, "This is message", "Message", MB_ICONEXCLAMATION | MB_YESNOCANCEL | MB_APPLMODAL );
-    std::cout << responce;
+    WNDCLASSEX wc;
+
+    wc.cbSize = sizeof( WNDCLASSEX );
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon( nullptr, IDI_APPLICATION );
+    wc.hCursor = LoadCursor( nullptr, IDC_ARROW );
+    wc.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = class_name;
+    wc.hIconSm = LoadIcon( nullptr, IDI_APPLICATION );
+
+    if ( !RegisterClassEx( & wc ) )
+    {
+        MessageBox( nullptr, "u dumb and ur code is not working", "womp womp",
+        MB_ICONEXCLAMATION | MB_OK );
+        return 1;
+    }
+
+    const HWND hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, class_name, "Window", WS_OVERLAPPEDWINDOW,
+                                     CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, nullptr, nullptr, hInstance, nullptr);
+
+    if ( hwnd == nullptr )
+    {
+        MessageBox( nullptr, "window is nor working", "lol", MB_ICONEXCLAMATION );
+        return 1;
+    }
+
+    ShowWindow( hwnd, nShowCmd );
+    UpdateWindow( hwnd );
+
+    while ( GetMessage( & message, NULL, 0, 0 ) )
+    {
+        TranslateMessage( & message );
+        DispatchMessage( & message );
+    }
+    return message.wParam;
+}
+
+LRESULT CALLBACK WndProc(const HWND hwnd, const UINT msg, const WPARAM wParam, const LPARAM lParam )
+{
+    switch ( msg )
+    {
+        case WM_CLOSE:
+            DestroyWindow( hwnd );
+        break;
+
+        case WM_DESTROY:
+            PostQuitMessage( 0 );
+        break;
+
+        default:
+            return DefWindowProc( hwnd, msg, wParam, lParam );
+    }
+
     return 0;
 }
