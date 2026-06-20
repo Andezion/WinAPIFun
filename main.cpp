769acee27e97b8eacf3263bdbc78543bd877ac07
Andezion <245122@edu.p.lodz.pl>
2026-06-20 14:08:05 +0200

    add: added basic stuff

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <windows.h>
+
+int WINAPI WinMain( HINSTANCE hinstance, HINSTANCE hprevInstance, LPSTR lpCmdLine, int nCmdShow )
+{
+    auto responce = MessageBox( NULL, "This is message", "Message", MB_ICONINFORMATION | MB_OKCANCEL );
+    std::cout << responce;
+    return 0;
+}
