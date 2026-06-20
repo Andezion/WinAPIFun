8af87bbab1e8ea9c36f43a8bb8069ce6de582741
Andezion <245122@edu.p.lodz.pl>
2026-06-20 14:36:14 +0200

    add: added new info

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int WINAPI WinMain( HINSTANCE hinstance, HINSTANCE hprevInstance, LPSTR lpCmdLine, int nCmdShow )
 {
-    auto responce = MessageBox( NULL, "This is message", "Message", MB_ICONINFORMATION | MB_YESNOCANCEL );
+    auto responce = MessageBox( NULL, "This is message", "Message", MB_ICONEXCLAMATION | MB_YESNOCANCEL );
     std::cout << responce;
     return 0;
 }
